50fc64e2aae5091bbaceb14910d72ca8397fc10b
belgort-clark <bruceelgort@Bruces-iMac-2.local>
2019-02-05 19:49:35 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ctec-121-lab-4
+# Module 5 - Lab No. 4
+
+- Open up the module-5-lab-4.py file and follow the instructions.
+- Once you have completed the lab, stage, commit and push it back to GitHub
---
+++ module-5-lab-4.py
@@ -0,0 +1,47 @@
+# YOUR NAME
+# Module 5 - Lab No. 4
+
+# Make sure you changed Line 1 to be your first and last name
+
+teacher = "Bruce Elgort"
+# Using string slicing print the first letters "Bruc" from the teacher variable
+
+
+# Using string slicing print out the last 2 letters from the teacher variable
+
+
+# Write a for loop that displays each character from the teacher variable on a separate line
+
+
+# Using the .count() string method, display the number of u's in the teacher variable and print it out
+
+
+# Using the .find() string method, find the location of the letter 'E' in the teacher variable and print it out
+
+
+# Replace the word 'Bruce' in the teacher variable with the word 'Lawrence' and print it out
+
+
+
+grades = [100,100,0,0,0,55,95,96,88,100,77,87]
+
+# Print out the length of the grades list using the approrpriate Python function learned in class
+
+
+
+# Write a for loop that iterates through the grades list and prints each list element on a new line
+
+
+
+# Using a for loop, the accumulator pattern and the grades list, calculate the avarage grade and print it out
+
+
+aRandomSentence = "I like turtles especially green ones"
+
+# Using the aRandomSentence variable, use the .split() string method to create a list called sentenceWords
+
+
+# Write a for loop to print out each of the words in the sentenceWords list
+
+
+# Save your work, stage, commit and push it to GitHub
